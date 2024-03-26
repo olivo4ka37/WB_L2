076821eb065995fe760f8a897be770e2b605d9ea
olivo4ka <Kostylev.egor.1337@gmail.com>
2024-03-26 18:16:18 +0300

    Den govna vushel, ya v tilte lol

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,1 +1,25 @@
-package dev06
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Недостаточно аргументов. Ожидается минимум 3 аргумента.")
+		os.Exit(1)
+	}
+
+	arg1 := os.Args[1]
+	arg2 := os.Args[2]
+	arg3 := os.Args[3]
+
+	fmt.Println("Аргумент 1:", arg1)
+	fmt.Println("Аргумент 2:", arg2)
+	fmt.Println("Аргумент 3:", arg3)
+}
+
+type flags struct {
+	fields string
+}
